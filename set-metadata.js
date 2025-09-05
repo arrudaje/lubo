d4c5395664652157b02964e59137ee8f4ddd2f69
João Arruda <arrudaje@gmail.com>
2025-09-05 16:38:07 +0200

    fix: update profile image and ID for accurate representation in index.html

---
+++ set-metadata.js
@@ -0,0 +1,8 @@
+// import { storage } from "./index.js";
+// import { }
+
+// const setMetadata = async () => {
+//   const items = await list(ref(storage, "portfolio"));
+// };
+
+// setMetadata();
