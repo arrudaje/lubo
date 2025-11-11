d4b89d41d6973eeda95d678b67a0c4a05e0096c9
João Arruda <arrudaje@gmail.com>
2025-11-11 02:47:12 +0100

    fix: add mobile visibility for Natal 2025 link in flags.js and update ID in index.html

--- flags.js
+++ flags.js
@@ -5,7 +5,9 @@ const getFlag = (flag) => getBoolean(remoteConfig, flag);
 
 fetchAndActivate(remoteConfig).then(() => {
     const natal = document.getElementById("natal");
+    const natal_mobile = document.getElementById("natal_mobile");
     if (getFlag('NATAL_2025')) {
         natal.classList.remove("hidden");
+        natal_mobile.classList.remove("hidden");
     }
 });
