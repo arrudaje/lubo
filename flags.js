1c6895c112c1128a804ac71e0891faaeb01c4716
João Arruda <arrudaje@gmail.com>
2025-11-11 01:05:53 +0100

    feat: implement swing animation for elements and update HTML structure for seasonal display

---
+++ flags.js
@@ -0,0 +1,11 @@
+import { remoteConfig } from "./index.js";
+import { fetchAndActivate, getBoolean } from "firebase/remote-config";
+
+const getFlag = (flag) => getBoolean(remoteConfig, flag);
+
+fetchAndActivate(remoteConfig).then(() => {
+    const natal = document.getElementById("natal");
+    if (getFlag('NATAL_2025')) {
+        natal.classList.remove("hidden");
+    }
+});
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+import { getRemoteConfig, fetchAndActivate, getBoolean } from "firebase/remote-config";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,3 +25,7 @@ export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
+export const remoteConfig = getRemoteConfig(app);
+remoteConfig.settings = {
+  minimumFetchIntervalMillis: 0,
+};
